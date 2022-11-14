c3f24334340dda30efc012097e9f295185de24eb
lamalicce <matthea834@gmail.com>
2022-11-14 23:41:45 +0100

    split biento

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,24 @@
 /*   By: haze <haze@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:59:44 by mthea             #+#    #+#             */
-/*   Updated: 2022/11/14 17:52:29 by haze             ###   ########.fr       */
+/*   Updated: 2022/11/14 23:39:49 by haze             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
-static char	**free_malloc(char **str, int pos)
+
+static void	free_malloc(char **str, int pos)
 {
-	while (pos > 0)
-	{
-		free(str[pos]);
-	}
-	return (0);
+	int	i;
+
+	i = 0;
+	while (i++ < pos)
+		free(str[i]);
+	free(str);
 }
+
 static int	ft_alloc_nb_col(const char *s, char c)
 {
 	int	i;
@@ -41,35 +44,35 @@ static int	ft_alloc_nb_col(const char *s, char c)
 	return (compteur);
 }
 
-static char **ft_alloc_totale(const char *s, char c, char **tab)
+static char	**ft_alloc_totale(const char *s, char c, char **tab)
 {
 	int		i;
-	int compteur;
-	int	j;
+	int		compteur;
+	int		j;
 
 	i = 0;
-	j = -1;
+	j = 0;
 	compteur = 0;
-	if (s[i] == c)
-		i++;
 	while (s[i])
 	{	
 		if (s[i] != c)
 			compteur++;
-		if (s[i - 1] != c && s[i] == c && s[i + 1] != c)
+		else if (s[i - 1] != c && i > 0)
 		{
-			tab[++j] = (char *) malloc(sizeof(char) * (compteur + 1));
-			compteur = 0;
+			tab[j] = malloc(sizeof(char) * (compteur + 1));
 			if (tab[j] == NULL)
-				return (free_malloc(tab, j - 1));
+				return (0);
+			compteur = 0;
+			j++;
 		}
+		if (s[i + 1] == '\0' && s[i] != c)
+			tab[j] = malloc(sizeof(char) * (compteur + 1));
 		i++;
 	}
-	if (compteur > 0)
-		tab[++j] = (char *) malloc(sizeof(char) * (compteur + 1));
 	return (tab);
 }
-static char **ft_placement_mots(const char *s, char c, char **tab)
+
+static char	**ft_placement_mots(const char *s, char c, char **tab)
 {
 	int	i;
 	int	j;
@@ -78,32 +81,47 @@ static char **ft_placement_mots(const char *s, char c, char **tab)
 	i = 0;
 	j = 0;
 	compteur = 0;
-	if (s[i] == c)
-		i++;
 	while (s[i])
 	{
 		if (s[i] != c)
-			tab[j][compteur] = s[i];
-		if (s[i - 1] != c && s[i] == c && s[i + 1] != c)
+			tab[j][compteur++] = s[i];
+		else if (s[i - 1] != c && i > 0)
 		{
 			tab[j][compteur] = 0;
 			j++;
-			compteur = -1;
+			compteur = 0;
 		}
+		if (s[i + 1] == '\0' && s[i] != c)
+			tab[j][compteur] = 0;
 		i++;
-		compteur++;
 	}
-	tab[j][compteur] = 0;
 	return (tab);
 }
-char **ft_split(char const *s, char c)
-{
-	char **tab;
 
-	tab = (char **) malloc(sizeof(char *) * ft_alloc_nb_col(s, c));
-	if (ft_alloc_totale(s, c, tab) == 0)
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	int		taille;
+
+	taille = ft_alloc_nb_col(s, c);
+	if (*s == NULL)
+	{
+		tab = malloc(sizeof(char *) * 1);
+		if (tab == NULL)
+			return (NULL);
+		tab[0] = 0;
+		return (tab);
+	}
+	tab = malloc(sizeof(char *) * (taille + 1));
+	if (tab == NULL)
 		return (0);
-	tab = ft_alloc_totale(s, c, tab);
-	tab = ft_placement_mots(s,c,tab);
+	if (ft_alloc_totale(s, c, tab) == NULL)
+	{
+		free(tab);
+		return (tab);
+	}
+		tab = ft_alloc_totale(s, c, tab);
+		tab = ft_placement_mots(s, c, tab);
+	tab[taille] = 0;
 	return (tab);
 }
