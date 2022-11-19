1a0db25db0fa60b4f4f9ea6fb612a4ca1f5c07fd
matthis-thea <100850648+matthis-thea@users.noreply.github.com>
2022-11-19 17:58:10 +0100

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,21 +6,19 @@
 /*   By: mthea <mthea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 18:16:27 by mthea             #+#    #+#             */
-/*   Updated: 2022/11/18 14:51:02 by mthea            ###   ########.fr       */
+/*   Updated: 2022/11/19 13:47:45 by mthea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t elementCount, size_t elementSize)
+void	*ft_calloc(size_t count, size_t size)
 {
-	int		i;
 	void	*pointeur;
 
-	i = 0;
-	pointeur = malloc(elementCount * elementSize);
+	pointeur = (void *) malloc(count * size);
 	if (!pointeur)
-		return (0);
-	ft_bzero(pointeur, (elementCount * elementSize));
+		return (NULL);
+	ft_bzero(pointeur, (count * size));
 	return (pointeur);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mthea <mthea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 11:42:48 by mthea             #+#    #+#             */
-/*   Updated: 2022/11/18 12:33:19 by mthea            ###   ########.fr       */
+/*   Updated: 2022/11/19 14:45:04 by mthea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *thestring);
-void	*ft_calloc(size_t elementCount, size_t elementSize);
+void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *source);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
